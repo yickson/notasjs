075d3c240b5762913e90f17f6c7f00b1e214df2c
Yickson <y.ramirez@redcapital.cl>
2019-11-01 19:22:30 -0300

    [UPD] Api

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios, axios)
 
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://backend-node-yick.herokuapp.com/api';
 
 Vue.config.productionTip = false
 
